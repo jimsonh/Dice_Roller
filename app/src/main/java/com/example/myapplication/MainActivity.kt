ea970ef4988adfa94c91de394647e13de543b3bc
jimsonh <jimsonho2010@hotmail.com>
2019-10-24 01:54:01 +0900

    Practical 1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.rollBtn)
+        val resetButton: Button = findViewById(R.id.resetBtn)
+        rollButton.setOnClickListener { rollDice() }
+        resetButton.setOnClickListener{ resetDice()}
+        diceImage = findViewById(R.id.dice_image)
+
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+
+
+
+
+    }
+
+    private fun resetDice() {
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        diceImage.setImageResource(R.drawable.empty_dice)
+
+
+
+    }
+
+
+
+
+}
